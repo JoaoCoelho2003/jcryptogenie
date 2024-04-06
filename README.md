6d792b473a688162106d81e6c044e80764d3ea2c
chico ferreira <36338391+chicoferreira@users.noreply.github.com>
2024-04-06 11:56:39 +0100

    feat: dioxius

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install
+
+```
+rustup target add wasm32-unknown-unknown
+cargo install dioxus-cli
+```
+
+# Run
+
+```bash
+dx serve --platform desktop --hot-reload
+```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,64 @@
+use axum::{extract::ws::WebSocketUpgrade, response::Html, Router, routing::get};
+use dioxus::prelude::*;
+
+use crate::history::fetch_exchange_rates;
+
 mod history;
 
-fn main() -> anyhow::Result<()> {
-    let rates = history::fetch_exchange_rates()?;
-    rates.iter().for_each(|rate| println!("{:?}", rate));
+fn app() -> Element {
+    let mut state = use_signal(|| fetch_exchange_rates().unwrap_or_default());
+    use_future(move || async move {
+        loop {
+            if let Ok(rates) = fetch_exchange_rates() {
+                state.set(rates);
+            }
+            tokio::time::sleep(std::time::Duration::from_millis(1)).await;
+        }
+    });
 
-    Ok(())
+    rsx! {
+        div {
+            "{state:?}"
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr: std::net::SocketAddr = ([127, 0, 0, 1], 3030).into();
+
+    let view = dioxus_liveview::LiveViewPool::new();
+
+    let app = Router::new()
+        .route(
+            "/",
+            get(move || async move {
+                Html(format!(
+                    r#"
+            <!DOCTYPE html>
+            <html>
+                <head> <title>Dioxus LiveView with axum</title>  </head>
+                <body> <div id="main"></div> </body>
+                {glue}
+            </html>
+            "#,
+                    glue = dioxus_liveview::interpreter_glue(&format!("ws://{addr}/ws"))
+                ))
+            }),
+        )
+        .route(
+            "/ws",
+            get(move |ws: WebSocketUpgrade| async move {
+                ws.on_upgrade(move |socket| async move {
+                    _ = view.launch(dioxus_liveview::axum_socket(socket), app).await;
+                })
+            }),
+        );
+
+    println!("Listening on https://{addr}");
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
