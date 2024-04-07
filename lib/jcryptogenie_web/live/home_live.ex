2365ee91bf45c61fa156d356fb8a92fc063d3209
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-04-07 06:38:39 +0100

    feat: request to api and live update

--- lib/jcryptogenie_web/live/home_live.ex
+++ lib/jcryptogenie_web/live/home_live.ex
@@ -1,11 +1,23 @@
-# lib/jcryptogenie_web/live/home_live.ex
 defmodule JcryptogenieWeb.HomeLive do
   use JcryptogenieWeb, :live_view
+  alias Jcryptogenie.Cryptodata
+  import JcryptogenieWeb.Components.Chart
 
   def mount(_params, _session, socket) do
-    {:ok, socket
-      |> assign(:crypto_coin, "BTC")
-      |> assign(:balance, 1000)}
+      schedule_update_value(socket)
   end
 
+  def handle_info(:update_value, socket) do
+    {:noreply, assign(socket, :current_value, {:ok, %{current_value: Cryptodata.fetch_data()}})}
+    schedule_update_value(socket)
+  end
+
+  defp schedule_update_value(socket) do
+    Process.send_after(self(), :update_value, 5000)
+    {:ok, socket
+      |> assign(:crypto_coin, "BTC")
+      |> assign(:balance, 1000)
+      |> assign(:current_value, {:ok, %{current_value: Cryptodata.fetch_data()}})
+    }
+  end
 end
--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Jcryptogenie.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:httpoison, "~> 1.8"}
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 5.0"}
     ]
   end
 
