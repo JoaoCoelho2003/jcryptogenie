f0b4df6671b417cbf7f2e80bb0cf6d4cda1dd173
João Coelho <jhcoelho03@gmail.com>
2024-04-07 11:58:00 +0100

    everything

--- lib/jcryptogenie/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Jcryptogenie.Mailer do
-  use Swoosh.Mailer, otp_app: :jcryptogenie
-end
--- lib/jcryptogenie_web/components/layouts.ex
+++
@@ -1,5 +0,0 @@
-defmodule JcryptogenieWeb.Layouts do
-  use JcryptogenieWeb, :html
-
-  embed_templates "layouts/*"
-end
